8f3ff1327f7a1fb45133393622a99839efa74c5f
David Ladowitz <david@ladowitz.com>
2016-12-17 15:28:04 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDe1gPq_hqwaMwLH9w3pdvzLnngvOcrnk4';
+
+const App = () => {
+  return <div>Hi!</div>;
+};
+
+// render App to DOM;
+ReactDOM.render(<App />, document.querySelector('.container'));
